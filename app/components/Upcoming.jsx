bb20c75d80f3b3d491a49e8f9e0cb71f5aad00e7
FrederickGodiva <godivafrederick@gmail.com>
2024-11-23 13:46:23 +0700

    fix: fix responsive upcoming flyer

--- app/components/Upcoming.jsx
+++ app/components/Upcoming.jsx
@@ -7,7 +7,7 @@ const Upcoming = () => {
       <h2 className="mb-16 text-4xl font-semibold text-center md:text-5xl">
         Upcoming Event
       </h2>
-      <div className="border-2 border-slate-900 w-[500px] h-[600px] flex items-center justify-center">
+      <div className="border-2 border-slate-900 md:w-[500px] md:h-[600px] h-80 w-64 flex items-center justify-center">
         <p className="font-semibold">Upcoming Event Poster</p>
       </div>
     </div>
